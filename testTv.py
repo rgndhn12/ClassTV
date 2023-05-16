5c6da1159b979df979033040b8e6565ab3b5f032
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-05-16 17:43:34 +0800

    printing the output

--- testTv.py
+++ testTv.py
@@ -1,8 +1,11 @@
 from ClassTV import Tv
 
 tv_one = Tv(30, 3, True)
-print(tv_one.get_channel())
-print(tv_one.set_channel())
+tv_two = Tv(3, 2, False)
+
+print(f"tv1's channel is {tv_one.get_channel()} and volume level is {tv_one.get_volume()}")
+print(f"tv2's channel is {tv_two.get_channel()} and volume level is {tv_two.get_volume()}")
+
 
 
 
