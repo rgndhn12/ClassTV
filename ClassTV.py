ca02a2f83c1c528b240224ca9cb5922978ce5112
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-05-16 15:56:06 +0800

    started on creating class with constructor

--- ClassTV.py
+++ ClassTV.py
@@ -1,5 +1,14 @@
 #Dahan, Regine Fae M. Dahan BSCPE 1-5 ( Introduction to OOP )
 
 #create a class TV
+
+class Tv:
+    #parameterized constructor
+    def __init__(self, channel, volumeLevel, power):
+        self.channel = channel 
+        self.volume_level = volumeLevel
+        self.power = power
+
+
 #make another file for instantiate
 #print the output
