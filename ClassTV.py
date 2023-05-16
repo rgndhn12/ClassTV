64220436a19ab974400768a9a27665d2a3dd4da6
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-05-16 16:59:21 +0800

    added volume up and down

--- ClassTV.py
+++ ClassTV.py
@@ -33,6 +33,11 @@ class Tv:
     def channel_down(self):
         self.channel -= 1
     
+    def volume_up(self):
+        self.volume_level += 1
+    
+    def volume_down(self):
+        self.volume_level -= 1
 
     
          
